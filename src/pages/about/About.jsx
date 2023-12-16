fc037c6bcf693aff22be43caa9f8c3182c39fbb6
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2023-12-16 17:00:04 +0545

    updated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -6,6 +6,7 @@ import MailList from "../../Components/mailList/MailList";
 
 const About = () => {
   return (
+    
     <div>
       <Navbar />
       <Header type="list" />
@@ -13,14 +14,14 @@ const About = () => {
       <div className="about-us">
         <h2>About Us</h2>
         <p>
-          Welcome to [Your Website Name], your gateway to unforgettable travel
+          Welcome to SnapSeat, your gateway to unforgettable travel
           experiences!
         </p>
 
         <section className="mission">
           <h3>Our Mission</h3>
           <p>
-            At [Your Website Name], our mission is to provide an effortless and
+            At SnapSeat, our mission is to provide an effortless and
             personalized booking experience for travelers. We offer a diverse
             selection of accommodations, ensuring everyone finds their perfect
             stay.
@@ -61,13 +62,12 @@ const About = () => {
         <section className="get-in-touch">
           <h3>Get in Touch</h3>
           <p>
-            Have any questions or need assistance? Contact us at [Contact
-            Information]. We're here to help!
+            Have any questions or need assistance? Contact us. We're here to help!
           </p>
         </section>
-        <MailList/>
+      </div>
+      <MailList />
       <Footer />
-      </div>     
     </div>
   );
 };
